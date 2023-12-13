e15da2c9887293e99f47c97cfe18b5eacef2b0df
dinanr <153698587+dinanr@users.noreply.github.com>
2023-12-13 10:53:57 +0700

    Add files via upload

---
+++ for/dowhile/biodata/main.js
@@ -0,0 +1,7 @@
+let nama = "Nama: Dina Norensa";
+let kelas = "Kelas: XI";
+let jurusan = "Jurusan: PPLG";
+let umur = "Umur: 17";
+let alamat = "Alamat: KP.GOBANG";
+
+document.write(nama + "<br>", kelas + "<br>", jurusan + "<br>", umur + "<br>", alamat + "<br>");
